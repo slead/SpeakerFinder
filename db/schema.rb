a6a10625b6f120522bbe1449f38d8a7803e975bb
Stephen Lead <stephen.lead@gmail.com>
2014-10-14 18:48:48 +1100

    added location columns to Speakers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005041635) do
+ActiveRecord::Schema.define(version: 20141014074512) do
 
   create_table "speakers", force: true do |t|
     t.string   "name"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20141005041635) do
     t.text     "about"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.float    "lat"
+    t.float    "lng"
   end
 
 end
