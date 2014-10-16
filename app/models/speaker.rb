cbb06041fda397671e41ed470a0e99fc2e225f87
Stephen Lead <stephen.lead@gmail.com>
2014-10-17 10:50:58 +1100

    remove Public Assets folder while debugging the Heroku missing assets problem

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,7 @@
 class Speaker < ActiveRecord::Base
-  validates_presence_of :name, :email, :city
+  validates_presence_of :name, :email, :city  
+  validates :website, :format => URI::regexp(%w(http https))
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
   
 end
