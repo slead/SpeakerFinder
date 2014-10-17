ee6e70ff4f6d121929fb5b9e338ab447af24f30f
Stephen Lead <stephen.lead@gmail.com>
2014-10-17 15:36:39 +1100

    rename lat/long fields to latitude/longitude to match Geocoder requirements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016070428) do
+ActiveRecord::Schema.define(version: 20141017043011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20141016070428) do
     t.string   "city"
     t.string   "state"
     t.string   "country"
-    t.float    "lat"
-    t.float    "lng"
+    t.float    "latitude"
+    t.float    "longitude"
     t.string   "image_url"
     t.string   "blurb"
   end
