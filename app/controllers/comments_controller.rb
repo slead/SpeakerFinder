924b1f5bf0bbe6659ccfa0b154c5a2b6bf7e6814
Stephen Lead <stephen.lead@gmail.com>
2014-10-26 15:30:21 +1100

    continue tweaking the comment display parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
       @comment.user_id = current_user.id
       @comment.save
       redirect_to speaker_path(@speaker)
+      
     end
     
     def destroy
